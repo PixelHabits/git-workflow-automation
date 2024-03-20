de7988e5af55f54eb4ded18587da31f04d2b2c16
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-03-20 01:39:41 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,110 @@
+# Git Workflow Automation Script
+
+This Bash script streamlines the Git workflow for developers by automating several common tasks associated with setting up, maintaining, and pushing changes to a Git repository. It's designed to save time and reduce the manual steps required during project setup and version control management.
+
+## Prerequisites
+
+Before you start, ensure you have the following prerequisites installed on your system:
+
+- Git
+- Node.js and npm (if your project requires them)
+- GitHub CLI
+
+## Features:
+
+- **Directory Confirmation:** Prompts the user to confirm they're working in the correct directory before proceeding with any Git operations, enhancing safety and accuracy.
+- **Project Download Option:** Offers the choice to download a project using either the `tiged` (npx tiged command) or `clone` method, facilitating easy project setup from a remote repository.
+- **Automatic Repository Creation:** Allows the creation of a new GitHub repository for the current directory. It initializes the repository as private by default, streamlining the process of going from a local project to a remote repository.
+- **`.gitignore` File Management:** Checks for the existence of a `.gitignore` file and offers to create one if it's missing, with a default entry to ignore `node_modules`, helping maintain a clean repository.
+- **Node Modules Installation:** Detects if the `node_modules` directory exists and offers to install dependencies using `npm install` if it doesn't, ensuring project dependencies are up to date.
+- **Git Initialization Check:** Verifies if the current directory is already initialized as a Git repository, and initializes one if not, ensuring that version control is set up.
+- **Staging and Committing Changes:** Prompts the user to stage all changes and commit them, with options for an "Initial Commit" message or a custom message, facilitating straightforward version control management.
+- **Git Status and Log Overview:** After committing, displays the current Git status and a log of commits, providing a clear overview of the repository's state.
+- **Remote Origin Management:** Checks if a remote origin is set and offers to add or change it, ensuring that the local repository is correctly linked to a remote one. This step includes pushing changes to the remote repository.
+- **Push Validation:** Before pushing, confirms with the user that everything looks good, adding an extra layer of verification to prevent accidental pushes.
+- **Script Exit Points:** Includes multiple exit points based on user decisions or if critical steps fail (e.g., directory change, Git repository initialization), ensuring that the script does not proceed without the required setup.
+
+## Usage:
+
+Ensure you have Git, npm (Node Package Manager), and the GitHub CLI installed on your system before using the script. If you haven't set up the alias `gitwf` for the script, please refer to the installation instructions provided with this project for details on how to do so. This alias allows for easy access and execution of the script from anywhere in your terminal.
+
+To use the script, simply type `gitwf` or the alias you setup into your terminal and press Enter while in the directory you would like your project to be downloaded or the directory of the project you want to run the above-mentioned functions on. Follow the interactive prompts to navigate through various Git and project setup tasks, such as confirming the current directory, downloading a project, creating a repository, managing `.gitignore`, installing node modules, initializing Git, staging and committing changes, and managing remote origins.
+
+This script is designed to improve the efficiency of Git workflows, making it easier for developers to manage their version control tasks and project setups this may not be a perfect fit for your use case, so if you have any suggestion for improvements feel free to submit a issue or pull request. I would to expand the functionality of this script.
+
+## Installation
+
+### Step 1: Install the GitHub CLI
+
+First, install the GitHub CLI (`gh`) if it's not already installed on your machine. Execute the following commands in your terminal:
+
+```sh
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+```
+
+### Step 2: Authenticate with GitHub
+
+Authenticate with your GitHub account using `gh`:
+
+```sh
+gh auth login
+```
+
+Follow the prompts to select your preferred authentication method and set the default git protocol.
+
+### Step 3: Download the Script
+
+Download `git_workflow.sh` to your machine. Place it in your home directory or one dedicated to scripts/utilities for easy access.
+
+### Step 4: Upgrade Permissions
+
+Ensure the script is executable by updating its permissions:
+
+```sh
+chmod +x /path/to/git_workflow.sh
+```
+
+For example, if placed in home directory as suggested the command would be:
+
+```sh
+chmod +x git_workflow.sh
+```
+
+Replace `/path/to/git_workflow.sh` with the actual file path.
+
+### Step 5: Add an Alias for the Script
+
+Add an alias to your terminal's profile file to easily access the script:
+
+```sh
+echo "alias gitwf='bash /path/to/git_workflow.sh'" >> ~/.bashrc
+```
+
+Adjust the path accordingly and then apply the changes:
+
+```sh
+source ~/.bashrc
+```
+
+## Contributing
+
+If you'd like to contribute to the development of the script, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch (`git checkout -b feature/AmazingFeature`).
+3. Make your changes and commit them (`git commit -m 'Add some AmazingFeature'`).
+4. Push to the branch (`git push origin feature/AmazingFeature`).
+5. Open a pull request.
+
+## License
+
+This project is licensed under the GNU General Public License v3.0 - see the [GPLv3 license](https://www.gnu.org/licenses/gpl-3.0.en.html) for details. The GNU GPLv3 is a free, copyleft license for software and other kinds of works, offering the freedom to use, modify, and distribute, ensuring that all modifications and derived works are also available under the same license.
+
+## Contact
+
+[PixelHabits](https://github.com/PixelHabits)
+
+Project Link: [https://github.com/PixelHabits/git-workflow-automation](https://github.com/PixelHabits/git-workflow-automation)
---
+++ git_workflow.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+
+# Function to check if the response is affirmative
+is_yes() {
+    [[ "$1" =~ ^[Yy][Ee]?[Ss]?$ ]]
+}
+
+# Function to check if the response is negative
+is_no() {
+    [[ "$1" =~ ^[Nn][Oo]?$ ]]
+}
+
+echo "Starting Git workflow automation..."
+echo "-----------------------------------"
+
+repo_url="" # Initialize an empty string to hold the repository URL
+
+# Function to confirm current directory
+confirm_directory() {
+    local current_dir_name=$(basename "$PWD")
+    local parent_dir_name=$(basename "$(dirname "$PWD")")
+    local prompt_message="Are you in the correct directory? ($parent_dir_name/$current_dir_name) (Yes/No): "
+    
+    read -p "$prompt_message" confirm_dir
+    if is_no "$confirm_dir"; then
+        echo "Please navigate to the correct directory and restart the script."
+        exit 1
+    fi
+}
+
+
+
+
+# Always confirm directory at the beginning
+confirm_directory
+
+# Ask if need to download a project
+read -p "Do you need to download a project? (Yes/No): " need_download
+if is_yes "$need_download"; then
+    echo "-----------------------------------"
+    read -p "Would you like to use 'tiged' or 'clone' to download the project? (tiged/clone): " download_method
+
+    if [[ "$download_method" == "tiged" ]]; then
+        read -p "Please enter the full 'npx tiged' command to download the project: " tig_command
+        eval $tig_command # Using eval to execute the command as it's entered
+        # Assuming the last argument is the directory name, which might not always be correct
+        dir_name=$(echo $tig_command | awk '{print $NF}')
+        cd "$dir_name" || { echo "Failed to change directory to $dir_name. Does it exist?"; exit 1; }
+    elif [[ "$download_method" == "clone" ]]; then
+        read -p "Please enter the Git repository URL to clone: " git_clone_url
+        repo_name=$(basename -s .git "$git_clone_url")
+        git clone "$git_clone_url"
+        cd "$repo_name" || { echo "Failed to change directory to $repo_name. Does it exist?"; exit 1; }
+    else
+        echo "Invalid option selected. Exiting script."
+        exit 1
+    fi
+    echo "Repository downloaded and changed to directory."
+    echo "-----------------------------------"
+else
+    echo "Okay, skipping project download."
+    echo "-----------------------------------"
+fi
+
+
+
+# Ask if need to create a repo
+read -p "Do you need to create a repo? (Yes/No): " need_repo
+if is_yes "$need_repo"; then
+    confirm_directory
+    current_dir=${PWD##*/}
+    echo "Current directory name is '$current_dir'."
+    echo "Creating a private GitHub repository named '$current_dir'."
+    repo_url=$(gh repo create "$current_dir" --private --confirm -y | grep "https://")
+    echo "GitHub repository created with URL: $repo_url"
+    echo "-----------------------------------"
+else
+    echo "Okay, skipping repo creation."
+    echo "-----------------------------------"
+    # Confirm current directory after potentially creating a repo
+    confirm_directory
+fi
+
+
+# Check for .gitignore file
+if [ ! -f .gitignore ]; then
+    echo "No .gitignore file found."
+    ls -a
+    echo "-----------------------------------"
+    read -p "Would you like to add a .gitignore file? (Yes/No): " add_gitignore
+    if is_yes "$add_gitignore"; then
+        echo "node_modules" > .gitignore
+        echo ".gitignore file created and node_modules added."
+    fi
+    echo "-----------------------------------"
+fi
+
+# Ask if user wants to install node_modules
+if [ -d "node_modules" ]; then
+    echo "The 'node_modules' directory already exists. Skipping installation."
+else
+    read -p "The 'node_modules' directory does not exist. Would you like to install node_modules? (Yes/No): " install_node
+    if is_yes "$install_node"; then
+        npm install
+        echo "node_modules installed."
+    else
+        echo "Skipping node_modules installation."
+    fi
+fi
+echo "-----------------------------------"
+
+
+# Checking if the repository has already been initialized
+if [ -d ".git" ]; then
+    echo "Git repository already initialized."
+else
+    git init || { echo "Failed to initialize Git repository."; exit 1; }
+    echo "Git repository initialized."
+fi
+echo "-----------------------------------"
+
+# Asking if the user is ready to add files to staging
+read -p "Are you ready to add files to staging? (Yes/No): " ready_to_add
+if is_yes "$ready_to_add"; then
+    git add . && echo "Added all files to staging."
+else
+    echo "Skipping adding files to staging."
+fi
+echo "-----------------------------------"
+
+# Asking if the user is ready to commit the changes & Commit changes with a message
+if is_yes "$ready_to_add"; then # Proceed if the user was ready to add
+    read -p "Are you ready to commit the changes? (Yes/No): " ready_to_commit
+    if is_yes "$ready_to_commit"; then
+        read -p "Is the commit message 'Initial Commit'? (Yes/No - Can be entered in the next step): " initial_commit
+        if is_yes "$initial_commit"; then
+            git commit -m "Initial Commit" || { echo "Commit failed."; exit 1; }
+            echo "Changes committed with the message: 'Initial Commit'"
+        else
+            read -p "Enter your commit message: " commit_message
+            git commit -m "$commit_message" || { echo "Commit failed."; exit 1; }
+            echo "Changes committed with message: '$commit_message'"
+        fi
+    else
+        echo "Skipping commit."
+    fi
+else
+    echo "Cannot proceed to commit without adding files to staging."
+fi
+echo "-----------------------------------"
+
+# Show git status and log
+git status
+echo "-----------------------------------"
+git log --oneline --graph --decorate --all
+echo "-----------------------------------"
+
+
+# Confirm if everything looks good
+read -p "Does everything above look good, I'm about to try pushing to the GitHub Repo? (Yes/No): " looks_good
+if is_yes "$looks_good"; then
+    echo "-----------------------------------"
+    if ! git remote get-url origin &> /dev/null; then
+        # This condition now implies either a new project or a cloned repo without an origin
+        if [ -n "$repo_url" ]; then
+            git remote add origin "$repo_url"
+            echo "Remote origin set to created repository URL."
+        else
+            read -p "Enter remote origin URL: " remote_origin_url
+            git remote add origin "$remote_origin_url" && echo "Remote origin added."
+        fi
+    else
+        # Remote origin exists, print it and ask the user if they want to change it
+        existing_origin_url=$(git remote get-url origin)
+        echo "The existing remote origin URL is: $existing_origin_url"
+        read -p "Would you like to keep the existing remote origin? (Yes/No): " keep_origin
+        if is_no "$keep_origin"; then
+            read -p "Enter new remote origin URL: " new_origin_url
+            git remote set-url origin "$new_origin_url"
+            echo "Remote origin has been updated to $new_origin_url."
+        else
+            echo "Keeping the existing remote origin: $existing_origin_url."
+        fi
+    fi
+    echo "-----------------------------------"
+    git remote -v
+    echo "-----------------------------------"
+    git push -u origin main || { echo "Push failed."; exit 1; }
+    echo "All done, check the repo!"
+else
+    echo "Please review your changes before proceeding."
+fi
+echo "-----------------------------------"
+echo "Workflow setup complete."
